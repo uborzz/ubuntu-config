9a1d19bd4f774ee07c331a751bf45915c5a68b31
uborzz <uborzz@gmail.com>
2021-10-17 12:06:04 +0200

    versions.sh updated

--- configure.sh
+++ configure.sh
@@ -151,7 +151,7 @@ sudo usermod -aG docker $USER
 
 # portainer 
 docker volume create portainer_data
-docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+sudo docker run -d -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
 
 
 # WIP: running in vscode python interpreters from dockerized venvs
--- versions.sh
+++ versions.sh
@@ -5,10 +5,11 @@ info () {
 }
 
 info MAIN
-git --version
-docker --version
+curl --version | head -1 | awk '{print $1,$2}'
 zsh --version
+git --version
 terminator --version
+echo vscode $(code --version | head -1)
 
 info PYTHON
 python --version
@@ -24,26 +25,48 @@ echo node $(node --version)
 info OTHER LANGUAJES
 rustc --version
 go version
-deno --version
+echo "$(deno --version | tr '\n' ' ')"
+echo "java $(java --version | head -1)"
+
+info CONTAINERS
+docker --version
+docker-compose --version
+az --version 2> /dev/null  | head -1
+echo kubectl $(kubectl version -o json | grep gitVersion | awk '{print $2}' | tr -d '",')
+echo helm $(helm version | grep -o "Version:[^,]*,\s") 
+helmfile --version
+kind --version
+minikube version | head -1
 
 info TOOLS
 vim --version | head -1
-docker-compose --version
-curl --version | head -1 | awk '{print $1" "$2}'
 thefuck --version
 echo tldr $(tldr --version)
 meld --version
 echo fzf $(fzf --version)
+tree --version
+nmap --version | head -1
+ccat --version
+cola --version
 
 info APPS
 google-chrome --version
 timeshift | head -2 | tail -1
+gimp --version
 
 info SNAPS
 snap list | grep -v canonical | awk '{print $1"\t"$2"\t"$5}' | column -t
 
 info VSCODE EXTENSIONS
 code --list-extensions
+
+info NOT CHECKED APPS
+echo parcellite
+echo joplin
+echo pdfmixtool
+echo xnview
+echo kazam
+echo openshot
 
 info SSH PUB KEY
 cat ~/.ssh/id_ed25519.pub
